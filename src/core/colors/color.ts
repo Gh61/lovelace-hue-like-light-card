27fe8004eb74c9a7b1bedab74065b58f864a8bd2
Gh61 <t.zenkner@email.cz>
2023-04-10 12:53:55 +0200

    Added color tests Issue #19

--- src/core/colors/color.ts
+++ src/core/colors/color.ts
@@ -19,14 +19,35 @@ export class Color {
     }
 
     /**
-     * Gets opacity of this color.
+     * @returns red color component of this color (value in range 0 - 255).
+     */
+    public getRed(): number {
+        return this._red;
+    }
+
+    /**
+     * @returns green color component of this color (value in range 0 - 255).
+     */
+    public getGreen(): number {
+        return this._green;
+    }
+
+    /**
+     * @returns blue color component of this color (value in range 0 - 255).
+     */
+    public getBlue(): number {
+        return this._blue;
+    }
+
+    /**
+     * @returns opacity of this color (value in range 0 - 1).
      */
     public getOpacity(): number {
         return this._opacity;
     }
 
     /**
-     * Returns relative luminance (0-255).
+     * @returns relative luminance (0-255).
      */
     public getLuminance(): number {
         return this._red * 0.299 + this._green * 0.587 + this._blue * 0.114;
@@ -44,24 +65,41 @@ export class Color {
     /**
     * Parses the given color string. Only supports color name, rgb(a) and hex format.
     */
-    private parse(color_id: string, allowNames = true): void {
-        if (color_id.startsWith('#')) {
-            color_id = color_id.substring(1);
-            if (color_id.length == 3) {
-                this._red = parseInt(color_id.substring(0, 1) + color_id.substring(0, 1), 16);
-                this._green = parseInt(color_id.substring(1, 2) + color_id.substring(1, 2), 16);
-                this._blue = parseInt(color_id.substring(2, 3) + color_id.substring(2, 3), 16);
-            } else if (color_id.length == 6) {
-                this._red = parseInt(color_id.substring(0, 2), 16);
-                this._green = parseInt(color_id.substring(2, 4), 16);
-                this._blue = parseInt(color_id.substring(4, 6), 16);
-            } else {
+    private parse(colorId: string, allowNames = true): void {
+        if (colorId.startsWith('#')) {
+            colorId = colorId.substring(1);
+
+            const isHex3 = colorId.length == 3;
+            const isHex6 = colorId.length == 6;
+
+            if (!isHex3 && !isHex6) {
                 throw new Error('Hex color format should have 3 or 6 letters');
             }
-        } else if (color_id.startsWith('rgb')) {
-            const parts = color_id.match(/^rgba?\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,?\s*(\d*(?:\.\d+\s*)?)\)$/);
+
+            // parse all chars to integers
+            const colorValues = [];
+            for (let i = 0; i < colorId.length; i++) {
+                const value = parseInt(colorId[i], 16);
+                if (isNaN(value))
+                    throw new Error(`Hex color format contains non hex characters - '${colorId[i]}'`);
+
+                colorValues.push(value);
+            }
+
+            if (isHex3) {
+                this._red =   colorValues[0] * 16 + colorValues[0];
+                this._green = colorValues[1] * 16 + colorValues[1];
+                this._blue =  colorValues[2] * 16 + colorValues[2];
+            } else if (isHex6) {
+                this._red =   colorValues[0] * 16 + colorValues[1];
+                this._green = colorValues[2] * 16 + colorValues[3];
+                this._blue =  colorValues[4] * 16 + colorValues[5];
+            }
+
+        } else if (colorId.startsWith('rgb')) {
+            const parts = colorId.match(/^rgba?\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,?\s*(\d*(?:\.\d+\s*)?)\)$/);
             if (!parts) {
-                throw new Error('Unrecognized color format rgb[a]: ' + color_id);
+                throw new Error('Unrecognized color format rgb[a](...): ' + colorId);
             } else {
                 // [ str, r, g, b, a|undefined ]
                 this._red = parseInt(parts[1]);
@@ -73,13 +111,13 @@ export class Color {
                 // small hack: https://stackoverflow.com/a/47355187/1341409
                 const ctx = document.createElement('canvas').getContext('2d');
                 if (ctx != null) {
-                    ctx.fillStyle = color_id;
+                    ctx.fillStyle = colorId;
                     this.parse(ctx.fillStyle, false); // standardized color format (hex)
                     return;
                 }
             }
 
-            throw new Error('Unrecognized color format: ' + color_id);
+            throw new Error('Unrecognized color format: ' + colorId);
         }
     }
 
---
+++ tests/colors.test.ts
@@ -0,0 +1,86 @@
+import { Color } from "../src/core/colors/color";
+
+
+describe("Color", () => {
+    it("too short hex", () => {
+        const s = "#32";
+        
+        expect(() => {
+            new Color(s);
+        }).toThrowError('Hex color format should have 3 or 6 letters');
+    });
+
+    it("simple hex", () => {
+        const s = "#abc";
+        const color = new Color(s);
+
+        expect(color.getRed()).toBe(170);
+        expect(color.getGreen()).toBe(187);
+        expect(color.getBlue()).toBe(204);
+        expect(color.getOpacity()).toBe(1);
+    });
+
+    it("invalid simple hex", () => {
+        const s = "#ffx";
+
+        expect(() => {
+            new Color(s);
+        }).toThrowError('Hex color format contains non hex characters - \'x\'');
+    });
+
+    it("full hex", () => {
+        const s = "#17f7ad";
+        const color = new Color(s);
+
+        expect(color.getRed()).toBe(23);
+        expect(color.getGreen()).toBe(247);
+        expect(color.getBlue()).toBe(173);
+        expect(color.getOpacity()).toBe(1);
+    });
+
+    it("too short full hex", () => {
+        const s = "#17fec";
+        
+        expect(() => {
+            new Color(s);
+        }).toThrowError('Hex color format should have 3 or 6 letters');
+    });
+
+    it("invalid full hex", () => {
+        const s = "#17fgad";
+        
+        expect(() => {
+            new Color(s);
+        }).toThrowError('Hex color format contains non hex characters - \'g\'');
+    });
+
+    it("unknown format", () => {
+        const s = "rga(84,68,43)";
+        
+        expect(() => {
+            new Color(s);
+        }).toThrowError();
+    });
+
+    it("rgb", () => {
+        const s = "rgb(1,234,56)";
+        const color = new Color(s);
+
+        expect(color.getRed()).toBe(1);
+        expect(color.getGreen()).toBe(234);
+        expect(color.getBlue()).toBe(56);
+        expect(color.getOpacity()).toBe(1);
+    });
+
+    it("rgb spaces", () => {
+        const s = "rgb( 240,           89 ,38 )";
+        const color = new Color(s);
+
+        expect(color.getRed()).toBe(240);
+        expect(color.getGreen()).toBe(89);
+        expect(color.getBlue()).toBe(38);
+        expect(color.getOpacity()).toBe(1);
+    });
+
+    // named colors cannot be tested, because in test environment is no document canvas
+});
